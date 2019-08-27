fc67f402f9fcfbdd3827f6103eec7acd15d448e1
Edgar Lee <edgarl@netflix.com>
2019-08-26 20:09:21 -0700

    Add totals for bandwidth

--- reports/aggregates.go
+++ reports/aggregates.go
@@ -16,24 +16,49 @@ package reports
 
 import "github.com/Netflix/p2plab/metadata"
 
-type bitswapPair struct {
+type uint64Pair struct {
 	single    uint64
 	aggregate *uint64
+}
+
+type int64Pair struct {
+	single    int64
+	aggregate *int64
+}
+
+type float64Pair struct {
+	single    float64
+	aggregate *float64
 }
 
 func ComputeAggregates(reportByNodeId map[string]metadata.ReportNode) metadata.ReportAggregates {
 	var aggregates metadata.ReportAggregates
 	for _, reportNode := range reportByNodeId {
-		singleBitswap := reportNode.Bitswap
+		bswap := reportNode.Bitswap
 
-		for _, pair := range []bitswapPair{
-			{singleBitswap.BlocksReceived, &aggregates.Totals.Bitswap.BlocksReceived},
-			{singleBitswap.DataReceived, &aggregates.Totals.Bitswap.DataReceived},
-			{singleBitswap.BlocksSent, &aggregates.Totals.Bitswap.BlocksSent},
-			{singleBitswap.DataSent, &aggregates.Totals.Bitswap.DataSent},
-			{singleBitswap.DupBlksReceived, &aggregates.Totals.Bitswap.DupBlksReceived},
-			{singleBitswap.DupDataReceived, &aggregates.Totals.Bitswap.DupDataReceived},
-			{singleBitswap.MessagesReceived, &aggregates.Totals.Bitswap.MessagesReceived},
+		for _, pair := range []uint64Pair{
+			{bswap.BlocksReceived, &aggregates.Totals.Bitswap.BlocksReceived},
+			{bswap.DataReceived, &aggregates.Totals.Bitswap.DataReceived},
+			{bswap.BlocksSent, &aggregates.Totals.Bitswap.BlocksSent},
+			{bswap.DataSent, &aggregates.Totals.Bitswap.DataSent},
+			{bswap.DupBlksReceived, &aggregates.Totals.Bitswap.DupBlksReceived},
+			{bswap.DupDataReceived, &aggregates.Totals.Bitswap.DupDataReceived},
+			{bswap.MessagesReceived, &aggregates.Totals.Bitswap.MessagesReceived},
+		} {
+			*pair.aggregate += pair.single
+		}
+
+		bandwidth := reportNode.Bandwidth.Totals
+		for _, pair := range []int64Pair{
+			{bandwidth.TotalIn, &aggregates.Totals.Bandwidth.Totals.TotalIn},
+			{bandwidth.TotalOut, &aggregates.Totals.Bandwidth.Totals.TotalOut},
+		} {
+			*pair.aggregate += pair.single
+		}
+
+		for _, pair := range []float64Pair{
+			{bandwidth.RateIn, &aggregates.Totals.Bandwidth.Totals.RateIn},
+			{bandwidth.RateOut, &aggregates.Totals.Bandwidth.Totals.RateOut},
 		} {
 			*pair.aggregate += pair.single
 		}
