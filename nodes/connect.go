04493059db31c8266ce1301b8d098103e7f8cc45
Dirk McCormick <dirkmdev@gmail.com>
2019-09-27 09:27:02 -0400

    wip: avoid extraneous network call

--- nodes/connect.go
+++ nodes/connect.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/Netflix/p2plab"
@@ -41,7 +42,8 @@ func Connect(ctx context.Context, ns []p2plab.Node) error {
 	zerolog.Ctx(ctx).Info().Msg("Retrieving peer infos")
 	go logutil.Elapsed(gctx, 20*time.Second, "Retrieving peer infos")
 
-	var peerInfos []peerstore.PeerInfo
+	var lk sync.Mutex
+	peerInfoByNodeID := make(map[string]peerstore.PeerInfo)
 	for _, n := range ns {
 		n := n
 		collectPeerAddrs.Go(func() error {
@@ -54,7 +56,9 @@ func Connect(ctx context.Context, ns []p2plab.Node) error {
 				return errors.Errorf("peer %q has zero addresses", n.Metadata().Address)
 			}
 
-			peerInfos = append(peerInfos, peerInfo)
+			lk.Lock()
+			peerInfoByNodeID[n.ID()] = peerInfo
+			lk.Unlock()
 
 			for _, ma := range peerInfo.Addrs {
 				zerolog.Ctx(gctx).Debug().Str("addr", ma.String()).Msg("Retrieved peer address")
@@ -75,21 +79,24 @@ func Connect(ctx context.Context, ns []p2plab.Node) error {
 	// each other can get random disconnects.
 	conns := make(map[peer.ID]map[peer.ID][]string)
 	for _, n := range ns {
-		npi, err := n.PeerInfo(ctx)
-		if err != nil {
-			return err
+		npi, ok := peerInfoByNodeID[n.ID()]
+		if !ok {
+			panic("Should have peer info for every node")
 		}
 
 		conns[npi.ID] = make(map[peer.ID][]string)
-		for _, pi := range peerInfos {
-			if _, ok := conns[pi.ID]; !ok && pi.ID != npi.ID {
-				var peerAddrs []string
-				for _, ma := range pi.Addrs {
-					peerAddrs = append(peerAddrs, fmt.Sprintf("%s/p2p/%s", ma, pi.ID))
-				}
-				if len(peerAddrs) > 0 {
-					conns[npi.ID][pi.ID] = peerAddrs
-				}
+		for _, pi := range peerInfoByNodeID {
+			_, ok := conns[pi.ID]
+			if ok || pi.ID == npi.ID {
+				continue
+			}
+
+			var peerAddrs []string
+			for _, ma := range pi.Addrs {
+				peerAddrs = append(peerAddrs, fmt.Sprintf("%s/p2p/%s", ma, pi.ID))
+			}
+			if len(peerAddrs) > 0 {
+				conns[npi.ID][pi.ID] = peerAddrs
 			}
 		}
 	}
@@ -98,24 +105,28 @@ func Connect(ctx context.Context, ns []p2plab.Node) error {
 	go logutil.Elapsed(gctx, 20*time.Second, "Connecting cluster")
 
 	for _, n := range ns {
-		npi, err := n.PeerInfo(ctx)
-		if err != nil {
-			return err
+		npi, ok := peerInfoByNodeID[n.ID()]
+		if !ok {
+			panic("Should have peer info for every node")
 		}
 
 		toConns, ok := conns[npi.ID]
-		if ok {
-			for _, peerAddrs := range toConns {
-				if len(peerAddrs) > 0 {
-					err := n.Run(ctx, metadata.Task{
-						Type:    metadata.TaskConnectOne,
-						Subject: strings.Join(peerAddrs, ","),
-					})
+		if !ok {
+			continue
+		}
 
-					if err != nil {
-						return err
-					}
-				}
+		for _, peerAddrs := range toConns {
+			if len(peerAddrs) == 0 {
+				continue
+			}
+
+			err := n.Run(ctx, metadata.Task{
+				Type:    metadata.TaskConnectOne,
+				Subject: strings.Join(peerAddrs, ","),
+			})
+
+			if err != nil {
+				return err
 			}
 		}
 	}
